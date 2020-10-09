36abaceea53d99c416a6117669bc7d6adac0f32d
pakrentos <n.smirnov@innopolis.university>
2020-10-09 12:50:43 +0300

    refactored for fastapi

--- namenode/namenode.py
+++ namenode/namenode.py
@@ -1,109 +1,88 @@
 from flask import Flask, request
 from . import database as db
 from werkzeug.exceptions import BadRequest
+from fastapi import HTTPException
 
-app = Flask(__name__)
+from typing import Optional
 
-def send_init():
-    pass
+from fastapi import FastAPI
+from fastapi.requests import Request
+from utils.serialize.namenode import *
 
-def check_request_data(data):
-    if data is None:
-        raise BadRequest()
 
-@app.route('/dfs_init', methods=['GET'])
-def init():
-    send_init()
-    size = db.get_available_size()
-    return {'size': size}
+# app = Flask(__name__)
 
-@app.route('/new_node', methods=['POST'])
-def add_new_node():
-    data = request.get_json()
-    check_request_data(data)
-    new_id = db.add_node(request.remote_addr(), data['size'])
-    return {'id': new_id}, 200
 
-@app.route('/dfs_file_create', methods=['POST'])
-def file_create():
-    data = request.get_json()
-    check_request_data(data)
-    db.create_file(data['filename'])
-    return {}, 200
 
-@app.route('/dfs_file_read', methods=['POST'])
-def file_read():
+# def check_request_data(data):
+#     if data is None:
+#         raise BadRequest()
+
+app = FastAPI()
+
+@app.get('/dfs_init', response_model=InitResponse)
+async def init():
+    # size = db.get_available_size()
+    return
+
+@app.post('/new_node', response_model=AddNodeResponse)
+async def add_new_node(node: AddNodeRequest, request:Request):
+    # new_id = db.add_node(request.client, node.available_storage)
+    return
+
+@app.post('/dfs_file_create', response_model=FileModel)
+async def file_create(file: frmf('sas')):
+    # db.create_file(file.path)
+    return
+
+@app.post('/dfs_file_read', response_model=FileModel)
+async def file_read(file: frmf('sos')):
     """
     TODO: add checking
     """
-    data = request.get_json()
-    check_request_data(data)
-    node = db.find_node_by_file(data['filename'])
-    return {'ip': node.addr}, 200
+    # node = db.find_node_by_file(file.path)
+    return
 
-@app.route('/dfs_file_write', methods=['POST'])
-def file_write():
-    data = request.get_json()
-    check_request_data(data)
-    size = data['size']
-    filename = data['filename']
-    ip = db.write_file(filename, size)
-    return {'ip': ip}, 200
+@app.post('/dfs_file_write', response_model=FileModel)
+async def file_write(file: frmf('ses', size=True)):
+    # ip = db.write_file(file)
+    return
 
-@app.route('/dfs_file_delete', methods=['POST'])
-def file_delete():
-    data = request.get_json()
-    check_request_data(data)
-    filename = data['filename']
-    ip = db.delete_file(filename)
-    return {}, 200
+@app.post('/dfs_file_delete', response_model=FileModel)
+async def file_delete(file: frmf('sus')):
+    # ip = db.delete_file(file)
+    return
 
-@app.route('/dfs_file_info', methods=['POST'])
-def file_info():
-    data = request.get_json()
-    check_request_data(data)
-    filename = data['filename']
-    file_info = db.get_file_info(filename)
-    return file_info, 200
+@app.post('/dfs_file_info', response_model=FileModel)
+async def file_info(file: frmf('sqs')):
+    # file_info = db.get_file_info(filename)
+    return
 
-@app.route('/dfs_file_copy', methods=['POST'])
-def file_copy():
-    data = request.get_json()
-    check_request_data(data)
-    filename = data['filename']
-    new_filename = data['cp_filename']
-    db.copy_file(filename, new_filename)
-    return {}, 200
+@app.post('/dfs_file_copy', response_model=FileModel)
+async def file_copy(file: frmf('sws', new_path=True)):
+    # filename = data['filename']
+    # new_filename = data['cp_filename']
+    # db.copy_file(filename, new_filename)
+    return FileModel()
 
-@app.route('/dfs_file_move', methods=['POST'])
-def file_move():
-    data = request.get_json()
-    check_request_data(data)
-    filename = data['filename']
-    new_filename = data['mv_filename']
-    db.move_file(filename, new_filename)
-    return {}, 200
 
-@app.route('/dfs_read_directory', methods=['POST'])
-def read_directory():
-    data = request.get_json()
-    check_request_data(data)
-    dirname = data['dirname']
-    info = db.dir_read(dirname)
-    return info, 200
+@app.post('/dfs_file_move', response_model=FileModel)
+async def file_move(file: frmf('srs', new_path=True)):
+    # filename = data['filename']
+    # new_filename = data['mv_filename']
+    # db.move_file(filename, new_filename)
+    return
 
-@app.route('/dfs_make_directory', methods=['POST'])
-def make_directory():
-    data = request.get_json()
-    check_request_data(data)
-    dirname = data['dirname']
-    db.dir_make(dirname)
-    return {}, 200
 
-@app.route('/dfs_delete_directory', methods=['POST'])
-def delete_directory():
-    data = request.get_json()
-    check_request_data(data)
-    dirname = data['dirname']
-    db.dir_delete(dirname)
-    return {}, 200
+@app.post('/dfs_read_directory', response_model=DirectoryModel)
+async def read_directory(dir: DirectoryRequestModel):
+    return
+
+
+@app.post('/dfs_make_directory', response_model=DirectoryModel)
+async def make_directory(dir: DirectoryRequestModel):
+    return
+
+@app.post('/dfs_delete_directory', response_model=DirectoryModel)
+async def delete_directory(dir: DirectoryRequestModel):
+    return

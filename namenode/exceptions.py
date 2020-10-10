230a0b5ccbc1525fc9484b727d4191dce42ff46e
pakrentos <n.smirnov@innopolis.university>
2020-10-10 15:11:56 +0300

    inroduced NOT_EMPTY_DIRECTORY error

--- namenode/exceptions.py
+++ namenode/exceptions.py
@@ -1,2 +1,32 @@
+from fastapi import HTTPException
+from config import *
 
 
+class NodeDisconnected(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=CODE_NODE_DISCONNECTED)
+
+
+class FileNotFound(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=CODE_FILE_NOT_FOUND)
+
+
+class NoSuchDirectory(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=CODE_NO_SUCH_DIRECTORY)
+
+
+class NotEnoughStorage(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=CODE_NOT_ENOUGH_STORAGE)
+
+
+class NoNodeAvailable(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=CODE_NO_NODE_AVAILABLE)
+
+
+class NotEmptyDirectory(HTTPException):
+    def __init__(self):
+        super().__init__(status_code=CODE_NOT_EMPTY_DIRECTORY)

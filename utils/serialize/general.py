fe0c43431b28579d5b8443ddd07e7795acfb0c39
pakrentos <n.smirnov@innopolis.university>
2020-10-10 03:58:16 +0300

    fixed StorageModel

--- utils/serialize/general.py
+++ utils/serialize/general.py
@@ -1,15 +1,24 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, conlist
+from typing import List, Type, Any, Optional
 from os import path
+from uuid import uuid4, UUID
 
 
 class StorageModel(BaseModel):
-    storage_id: int
+    storage_id: str
     storage_ip: str
     available_size: int
 
     class Config:
         orm_mode = True
+
+    def __init__(self, *args, **kwargs):
+        if kwargs.get('storage_id', None) is None:
+            kwargs['storage_id'] = str(uuid4())
+        elif isinstance(kwargs['storage_id'], UUID):
+            kwargs['storage_id'] = str(kwargs['storage_id'])
+        super().__init__(*args, **kwargs)
+
 
 class StorageListModel(BaseModel):
 
@@ -46,3 +55,6 @@ class DirectoryModel(BaseFileModel):
 
     def _files(self):
         return [path.dirname(file.path) for file in self.files]
+
+
+

301d3c807cfbac017bab35533073aaa88fa1c7d8
pakrentos <n.smirnov@innopolis.university>
2020-10-12 01:43:41 +0300

    fixes

--- namenode/app.py
+++ namenode/app.py
@@ -1,29 +1,70 @@
 from fastapi.requests import Request
 from utils.serialize.namenode import *
 from fastapi import FastAPI
-from .database import *
-from .storage_conn import *
+from fastapi.middleware.cors import CORSMiddleware
+from namenode.database import *
+from namenode.storage_conn import *
 from uuid import uuid4
-from .exceptions import *
+from namenode.exceptions import *
 import os
+from typing import Dict
+import uvicorn
 
 app = FastAPI()
 
+Base.metadata.bind = db_engine
+Base.metadata.drop_all()
+Base.metadata.create_all()
+Directory.create('/')
 
-@app.get('/dfs_init', response_model=InitResponse)
+origins = [
+    "http://localhost*",
+    "http://localhost:8080*"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# if __name__ == '__main__':
+#     uvicorn.run(app, host="0.0.0.0", port=8000, log_config=log_config)
+
+
+# @app.on_event("startup")
+# async def startup_event():
+#     handler = logging.StreamHandler()
+#     handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
+#     logger.addHandler(handler)
+
+
+@app.post('/dfs_init', response_model=InitResponse)
 async def init():
     db_init()
     send_init_all()
     storage_list = Node.get_node_models()
-    resp = InitResponse(available_size=storage_list.available_size())
+    resp = InitResponse(size=storage_list.available_size())
     return resp
 
 
 @app.post('/new_node', response_model=AddNodeResponse)
 async def add_new_node(node: AddNodeRequest, request: Request):
-    model = StorageModel(storage_id=uuid4(), storage_ip=request.client.host, available_size=node.available_storage)
-    Node.create(model)
-    return AddNodeResponse(storage_id=model.storage_id)
+    if Node.q().filter(Node.storage_ip == request.client.host).count() == 0:
+        storage_id = str(uuid4())
+        print()
+        model = StorageModel(storage_id=storage_id, storage_ip=request.client.host, available_size=node.available_storage)
+        Node.create(model)
+    else:
+        node = Node.q().filter(Node.storage_ip == request.client.host).scalar()
+        storage_id = node.storage_id
+        logger.info(f'Node {node.storage_id} up!')
+        paths = node.up()
+        if paths is not None:
+            for path in paths:
+                post(node.storage_ip, 'delete', Status, json={'path': path})
+    return AddNodeResponse(storage_id=storage_id)
 
 
 @app.post('/dfs_file_create', response_model=FileModel)
@@ -31,6 +72,8 @@ async def file_create(file: frmf('FileCreateRequest')):
     ping_n_repl()
     if not Directory.exist(os.path.dirname(file.path)):
         raise NoSuchDirectory()
+    if Directory.exist(file.path):
+        raise PathIsDirectory()
     resp = create_file(file)
     return resp
 
